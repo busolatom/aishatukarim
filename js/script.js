01d6c72a75d2dc7dd781f3e0bfeced7d416df63a
Busola Tom <busolatom@gmail.com>
2024-11-25 18:38:18 +0000

    vercel serverless backend setup

--- js/script.js
+++ js/script.js
@@ -1,24 +1,20 @@
-const API_KEY = process.env.GOOGLE_API_KEY; 
-const SPREADSHEET_ID = process.env.SHEET_ID; 
-const range = 'Sheet1!A2:D5'; // Update the range based on your sheet
-
-// Function to fetch data from Google Sheets
+// Function to fetch data from Google Sheets through backend
 function fetchContent() {
-    fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/${range}?key=${API_KEY}`)
+    fetch('https://aishatukarim.vercel.app/api/server')  // Make a request to backend API
         .then(response => response.json())
         .then(data => {
-            const rows = data.values;
+            const rows = data.data;
             // Loop through each row and populate the content
             rows.forEach(row => {
                 const pageName = row[0];
                 const content = row[1];
                 const description = row[2];
-                const imageURL = row[3]; // Assuming image URL is in column C for homepage
+                const imageURL = row[3]; 
 
                 if (pageName === 'Home') {
                     document.getElementById('homepage-title').innerText = content;
-                    document.getElementById('homepage-description').innerText = row[2]; // Description
-                    document.getElementById('homepage-image').src = row[3]; // Image URL
+                    document.getElementById('homepage-description').innerText = description; // Description
+                    document.getElementById('homepage-image').src = imageURL;
                 } else if (pageName === 'About Us') {
                     document.getElementById('about-content').innerText = content;
                 } else if (pageName === 'Services') {
