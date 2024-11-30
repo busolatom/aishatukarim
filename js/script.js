4898ef517c717f9253938565f473cc82d730b684
Busola Tom <busolatom@gmail.com>
2024-11-30 16:30:51 +0000

    review swiper added

--- js/script.js
+++ js/script.js
@@ -129,3 +129,22 @@ function getFallbackImage(productIndex) {
 
 // Fetch the content when the page loads
 document.addEventListener('DOMContentLoaded', fetchContent);
+
+// Swiper js
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  
